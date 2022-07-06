1b35dc4bfed5899c7e378e156e6bed9987a0e15d
Lucas Golle <lucasgolle@pop-os.localdomain>
2022-07-06 09:17:05 -0300

    fix: repository

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,11 @@
 import Router from "express";
 import userCreateController from "../controllers/userCreate.controller";
+import userListController from "../controllers/userList.controller";
 
 const userRoutes = Router();
 
 userRoutes.post("", userCreateController);
 
-userRoutes.get("", );
+userRoutes.get("", userListControllera);
 
 export default userRoutes;
